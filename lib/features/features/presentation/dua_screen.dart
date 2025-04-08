9f593f9231450bdf426ea512297c2766498058c1
alazad214 <alazad214@gmail.com>
2025-04-08 22:14:44 +0600

    dua screen, added

---
+++ lib/features/features/presentation/dua_screen.dart
@@ -0,0 +1,64 @@
+import 'package:din_guide_app/common_widgets/auth_appbar.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/dua_list.dart';
+
+class DuaScreen extends StatelessWidget {
+  const DuaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F9),
+      appBar: const CustomAppbar(title: 'দোয়া সমূহ', leadingVisible: true),
+      body: ListView.builder(
+        itemCount: duas.length,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        itemBuilder: (context, index) {
+          final dua = duas[index];
+          return Container(
+            margin: const EdgeInsets.only(bottom: 12),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              border: Border.all(color: Colors.grey.shade300),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    dua["arabic"]!,
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+                Text(
+                  dua["pronunciation"]!,
+                  textAlign: TextAlign.start,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  dua["bangla"]!,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(fontSize: 16, color: Colors.black87),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
